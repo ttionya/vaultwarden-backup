b965c42bb1b12c54428752cc4118f5d7dd0657d2
ttionya <git@ttionya.com>
2025-10-23 20:18:38 +0800

    test: more longer test separator

--- tests/test.sh
+++ tests/test.sh
@@ -82,14 +82,14 @@ function test_result() {
 
     if [[ "$2" == "0" ]]; then
         color green "Test case \"$1\" passed"
-        color none "${SEP}"
+        color none "${SEP}${SEP}"
         return
     fi
 
     ((ERROR_NUM++))
 
     color red "Test case \"$1\" failed"
-    color none "${SEP}"
+    color none "${SEP}${SEP}"
 }
 
 . tests/units/env-priority/test.sh
